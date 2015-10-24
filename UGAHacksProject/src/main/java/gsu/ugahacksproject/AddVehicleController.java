691180d800387d1f04d568af457564510eef8c29
ciwasaki1 <ciwasaki1@student.gsu.edu>
2015-10-24 01:39:44 -0400

    change addvehicle
    .

--- UGAHacksProject/src/main/java/gsu/ugahacksproject/AddVehicleController.java
+++ UGAHacksProject/src/main/java/gsu/ugahacksproject/AddVehicleController.java
@@ -5,9 +5,17 @@
  */
 package gsu.ugahacksproject;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +24,26 @@ import javafx.fxml.Initializable;
  */
 public class AddVehicleController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button frontbtn;
+    
+    @FXML
+    private void OpenUpload(ActionEvent event) throws IOException {
+        System.out.println("You clicked me!");
+        Stage stage; 
+        Parent root;
+
+        stage=(Stage) frontbtn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("text.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+
+    }   
     
 }
