57e2fafc5303a940e5a18e24394de8e6b1c99007
ciwasaki1 <ciwasaki1@student.gsu.edu>
2015-10-25 05:09:57 -0400

    YAAAAAAASSSSSSS

--- UGAHacksProject/src/main/java/gsu/ugahacksproject/MainApp.java
+++ UGAHacksProject/src/main/java/gsu/ugahacksproject/MainApp.java
@@ -17,7 +17,6 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
         //scene.getStylesheets().add("/styles/Styles.css");
         
-        stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
     }
---
+++ UGAHacksProject/src/main/java/gsu/ugahacksproject/PredictionVariables.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gsu.ugahacksproject;
+
+public class PredictionVariables {
+    public static String frontstatus;
+    public static String backstatus;
+    public static String driverstatus;
+    public static String passstatus;
+    
+    public static String frontURL;
+    public static String backURL;
+    public static String driverURL;
+    public static String passURL;
+}
